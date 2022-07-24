dfa499d04b5a78947a2002c1c7c8f84c496a6839
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-24 16:30:57 +0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,19 @@
 import React from "react";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Avatar,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from "@mui/icons-material";
 
 const Navbar = () => {
   return <div>Navbar</div>;
