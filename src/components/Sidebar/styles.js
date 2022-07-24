c4dd569376bd2e4391fbb2b5789715c03bdf2ad7
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-24 17:49:41 +0300

    Update styles.js

---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) => ({
+  imageLink: {
+    display: "flex",
+    justifyContent: "center",
+    padding: "10% 0",
+  },
+  image: {
+    width: "70%",
+  },
+}));
