2b548e365eec34f4f9e380c086c357870ed5f17c
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-24 16:45:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+
+import useStyles from "./styles";
+import { Actors, MovieInformation, Movies, Navbar, Profile } from ".";
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path={["/", "/approved"]}>
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actor/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 };
 
 export default App;
