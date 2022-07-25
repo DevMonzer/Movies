df3e08a6f87a4f445331a613645832853fcacbf1
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 10:41:39 +0300

    Update TMDB.js

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -7,6 +7,11 @@ export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
   endpoints: (builder) => ({
+    //* Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+
     //* Get Movies by Type
     getMovies: builder.query({
       query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
