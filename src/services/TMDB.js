ff0b0940fbfccdfbe075cb50d4a3341ccc1689b4
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 10:06:18 +0300

    Update TMDB.js

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,6 +1,17 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
+  endpoints: (builder) => ({
+    //* Get Movies by Type
+    getMovies: builder.query({
+      query: () => `movie/popular?page=${page}&api_key=${tmdbApi}`,
+    }),
+  }),
 });
+
+export const { useGetMoviesQuery } = tmdbApi;
