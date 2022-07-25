89da99c0ac257a82a2501c28b9bab78eca4e4398
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 10:20:03 +0300

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Grid } from "@mui/material";
+
+import useStyles from "./styles";
+import { Movie } from "..";
 
 const MovieList = () => {
-  return <div>MovieList</div>;
+  return (
+    <Grid container className={classes.movieContainer}>
+      {movies.results.slice(startFrom, numberOfMovies).map((movie, i) => (
+        <Movie key={i} movie={movie} i={i} />
+      ))}
+    </Grid>
+  );
 };
 
 export default MovieList;
