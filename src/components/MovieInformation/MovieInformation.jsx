4d47984d8935d7dc439b3e5bca58d6125a78b608
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 18:46:46 +0300

    Update MovieInformation.jsx

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,4 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Modal,
+  Typography,
+  Button,
+  ButtonGroup,
+  Grid,
+  Box,
+  CircularProgress,
+  Rating,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Movie as MovieIcon,
+  Theaters,
+  Language,
+  PlusOne,
+  Favorite,
+  FavoriteBorderOutlined,
+  Remove,
+  ArrowBack,
+} from "@mui/icons-material";
+import { Link, useParams, userParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 const MovieInformation = () => {
   return <div>MovieInformation</div>;
