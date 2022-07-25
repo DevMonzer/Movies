eaac95d4ba928559025d2b83070bd6b06b2e4b07
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 09:50:00 +0300

    create a store

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import { Provider } from "react-redux";
 
 import App from "./components/App";
+import store from "./app/store";
+
 import "./index.css";
 
 const theme = createTheme({});
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <ThemeProvider theme={theme}>
       <BrowserRouter basename="/movies">
         <App />
