3be7de9ab42b1ce52abd1c13926a2c3b904527c3
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 17:22:49 +0300

    Update currentGenreOrCategory.js

--- src/features/currentGenreOrCategory.js
+++ src/features/currentGenreOrCategory.js
@@ -11,11 +11,16 @@ export const genreOrCategory = createSlice({
     selectGenreOrCategory: (state, action) => {
       //   console.log(action.payload);
       state.genreIdOrCategoryName = action.payload;
-      // state.searchQuery = '';
+      state.searchQuery = "";
+    },
+
+    searchMovie: (state, action) => {
+      // console.log("searchMovie feature: ", action.payload);
+      state.searchQuery = action.payload;
     },
   },
 });
 
-export const { selectGenreOrCategory } = genreOrCategory.actions;
+export const { selectGenreOrCategory, searchMovie } = genreOrCategory.actions;
 
 export default genreOrCategory.reducer;
