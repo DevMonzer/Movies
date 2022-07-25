69acbda07ea6192b55fc85e37800ec428f87d60e
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 10:36:03 +0300

    Update Movie.jsx

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -9,9 +9,27 @@ const Movie = ({ movie, i }) => {
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-      <Typography className={classes.title} variant="h5">
-        {movie.title}
-      </Typography>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          <img
+            alt={movie.title}
+            className={classes.image}
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : "https://fillmurray.com/200/300"
+            }
+          />
+          <Typography className={classes.title} variant="h5">
+            {movie.title}
+          </Typography>
+          <Tooltip disableTouchListener title={`${movie.vote_average} / 10`}>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </Link>
+      </Grow>
     </Grid>
   );
 };
