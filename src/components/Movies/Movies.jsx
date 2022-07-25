f52844f53a72655228276b476c1612a4b75a27d8
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 10:29:51 +0300

    Update Movies.jsx

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -12,8 +12,30 @@ import { useGetMoviesQuery } from "../../services/TMDB";
 import { MovieList } from "..";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
+  // console.log(data);
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItem="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match the name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return "An error has occured.";
 
   return (
     <div>
