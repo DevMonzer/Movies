b83ffd4b086edd742a90980a826576cf0bafdc21
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 18:40:26 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+
+import { Box, Typography, Button } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
 
 import { userSelector } from "../../features/auth";
 
 const Profile = () => {
   const { user } = useSelector(userSelector);
 
-  return <div>Profile - {user.username}</div>;
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/filmpire";
+  };
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default Profile;
