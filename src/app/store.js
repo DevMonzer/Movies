16c1615e7f2bc2a50426b87f163fa593a905964f
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 10:09:59 +0300

    update store and movies

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { tmdbApi } from "../services/TMDB";
+
 export default configureStore({
-  reducer: {},
+  reducer: {
+    [tmdbApi.reducerPath]: tmdbApi.reducer,
+  },
 });
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { useGetMoviesQuery } from "../../services/TMDB";
 
 const Movies = () => {
+  const { data } = useGetMoviesQuery();
+  console.log(data);
+
   return <div>Movies</div>;
 };
 
