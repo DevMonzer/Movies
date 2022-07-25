38694f0b1e657d03db785118dff70d58b58e4ea2
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 10:29:04 +0300

    Update Movie.jsx

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -6,7 +6,14 @@ import useStyles from "./styles";
 
 const Movie = ({ movie, i }) => {
   const classes = useStyles();
-  return <div>Movie</div>;
+
+  return (
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
+      <Typography className={classes.title} variant="h5">
+        {movie.title}
+      </Typography>
+    </Grid>
+  );
 };
 
 export default Movie;
