7e93c4410a2d98b726211acc2bcb098939cfdd2e
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 17:19:42 +0300

    Update Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,7 +4,14 @@ import { Search as SearchIcon } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 
+import useStyles from "./styles";
+
 const Search = () => {
+  const classes = useStyles();
+  const [query, setQuery] = useState("");
+  const dispatch = useDispatch();
+  const location = useLocation();
+
   return <div>Search</div>;
 };
 
