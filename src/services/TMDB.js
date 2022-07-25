d1b3f1124acef49d43da98b1da11980223e82b12
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-25 09:52:58 +0300

    Create TMDB.js

---
+++ src/services/TMDB.js
@@ -0,0 +1,6 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const tmdbApi = createApi({
+  reducerPath: "tmdbApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
+});
