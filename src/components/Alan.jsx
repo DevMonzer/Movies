64b01e00022c1453bdeb5e2d37f2f10b502fc137
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 22:10:51 +0300

    Update Alan.jsx

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -10,8 +10,8 @@ const useAlan = () => {
     alanBtn({
       key: "e5e339dd5b9299e985263542f9802ead2e956eca572e1d8b807a3e2338fdd0dc/stage",
       onCommand: ({ command, mode }) => {
-        if (command === "changeMode") {
-          if (mode === "light") {
+        if (command == "changeMode") {
+          if (mode == "light") {
             setMode("light");
           } else {
             setMode("dark");
