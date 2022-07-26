f13af660111343b3f0d8d077764eafdaa9ba8535
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 17:49:27 +0300

    Update styles.js

--- src/components/RatedCards/styles.js
+++ src/components/RatedCards/styles.js
@@ -8,4 +8,12 @@ export default makeStyles((theme) => ({
       width: "100%",
     },
   },
+  searchInput: {
+    color: theme.palette.mode === "light" && "black",
+    filter: theme.palette.mode === "light" && "invert(1)",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "-10px",
+      marginBottom: "10px",
+    },
+  },
 }));
