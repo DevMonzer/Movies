34649240384ac17de59c29c2595b474fc9dd78a6
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 10:16:55 +0300

    Update Actors.jsx

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Button, CircularProgress, Grid, Typography } from "@mui/material";
+import { useHistory, useParams } from "react-router-dom";
+import { ArrowBack } from "@mui/icons-material";
+
+import useStyles from "./styles";
+import {
+  useGetActorDetailQuery,
+  useGetMoviesByActorIdQuery,
+} from "../../services/TMDB";
 
 const Actors = () => {
   return <div>Actors</div>;
