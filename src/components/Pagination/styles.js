f8776fd44ac045d84154749aeb132f469a43b400
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 10:38:30 +0300

    Update styles.js

---
+++ src/components/Pagination/styles.js
@@ -0,0 +1,8 @@
+import { makeStyles } from "@mui/styles";
+export default makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
