d0f6d346ee4a3942eaec0aa85f64925fe799e594
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 17:46:37 +0300

    Update RatedCards.jsx

--- src/components/RatedCards/RatedCards.jsx
+++ src/components/RatedCards/RatedCards.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Typography, Box } from "@mui/material";
 
-const RatedCards = () => {
-  return <div>RatedCards</div>;
+import useStyles from "./styles";
+import { Movie } from "..";
+
+const RatedCards = ({ title, data }) => {
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <Typography variant="h5" gutterBottom>
+        {title}
+      </Typography>
+      <Box display="flex" flexWrap="wrap" className={classes.container}>
+        {data?.results.map((movie, i) => (
+          <Movie key={movie.id} movie={movie} i={i} />
+        ))}
+      </Box>
+    </Box>
+  );
 };
 
 export default RatedCards;
