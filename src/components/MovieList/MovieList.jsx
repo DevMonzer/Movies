4095b4497081e606c1dee6afbc86338d83f655ab
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 10:32:29 +0300

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,13 +4,13 @@ import { Grid } from "@mui/material";
 import useStyles from "./styles";
 import { Movie } from "..";
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, numberOfMovies }) => {
   // console.log(movies);
   const classes = useStyles();
 
   return (
     <Grid container className={classes.movieContainer}>
-      {movies.results.map((movie, i) => (
+      {movies.results.slice(startFrom, numberOfMovies).map((movie, i) => (
         <Movie key={i} movie={movie} i={i} />
       ))}
     </Grid>
