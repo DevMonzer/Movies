8e36b1f5b7125de18957b9d472947d431198be8a
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 10:39:00 +0300

    Update styles.js

--- src/components/Pagination/styles.js
+++ src/components/Pagination/styles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from "@mui/styles";
+
 export default makeStyles((theme) => ({
   container: {
     display: "flex",
@@ -7,5 +8,9 @@ export default makeStyles((theme) => ({
   },
   button: {
     margin: "30px 2px",
+  },
+  pageNumber: {
+    margin: "0 20px !important",
+    color: theme.palette.text.primary,
   },
 }));
