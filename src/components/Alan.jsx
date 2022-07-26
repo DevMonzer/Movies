3cc39be53759562382e11ecd59ea1fdfcec01b50
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 21:57:53 +0300

    Update Alan.jsx

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { useEffect, useContext } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
+import { ColorModeContext } from "../utils/ToggleColorMode";
+
 const Alan = () => {
+  const { setMode } = useContext(ColorModeContext);
+
   useEffect(() => {
     alanBtn({
       key: "e5e339dd5b9299e985263542f9802ead2e956eca572e1d8b807a3e2338fdd0dc/stage",
