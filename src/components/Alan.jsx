f6a79c05647173f874d7b91f59da5faa177c878a
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-26 21:55:40 +0300

    add a new component

---
+++ src/components/Alan.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const Alan = () => {
+  useEffect(() => {
+    alanBtn({
+      key: "e5e339dd5b9299e985263542f9802ead2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        if (commandData.command === "go:back") {
+          // Call the client code that will react to the received command
+        }
+      },
+    });
+  }, []);
+
+  return <div>Alan</div>;
+};
+
+export default Alan;
