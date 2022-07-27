d754696e280331d41fc5f65f3b8146d944e9b5ca
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-27 13:39:48 +0300

    update gitignore and tmdb

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -7,6 +7,7 @@ export const tmdbApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
   endpoints: (builder) => ({
     //* Get Genres
+
     getGenres: builder.query({
       query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
     }),
