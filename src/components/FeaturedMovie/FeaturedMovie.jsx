f55f0c961e516af7360983f8fa787236be5e3a5b
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-27 11:18:08 +0300

    Update FeaturedMovie.jsx

--- src/components/FeaturedMovie/FeaturedMovie.jsx
+++ src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import {
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  Rating,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
-const FeaturedMovie = () => {
+import useStyles from "./styles";
+
+const FeaturedMovie = ({ movie }) => {
+  const classes = useStyles();
+
   return <div>FeaturedMovie</div>;
 };
 
