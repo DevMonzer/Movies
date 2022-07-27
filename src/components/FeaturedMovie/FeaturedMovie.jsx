cd6e3fee6ee35603c20ddb54d926e503860f2a70
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-27 11:25:16 +0300

    Update FeaturedMovie.jsx

--- src/components/FeaturedMovie/FeaturedMovie.jsx
+++ src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -48,6 +48,7 @@ const FeaturedMovie = ({ movie }) => {
                 {movie?.vote_average}/10{" "}
               </Typography>
             </Box>
+            <Typography variant="body2">{movie.overview}</Typography>
           </CardContent>
         </Box>
       </Card>
