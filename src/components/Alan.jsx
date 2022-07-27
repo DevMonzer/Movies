04c1b810ab128c40a31d846519c001b5080f6b5c
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-27 10:19:12 +0300

    Update Alan.jsx

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -18,10 +18,10 @@ const useAlan = () => {
   useEffect(() => {
     alanBtn({
       key: "e5e339dd5b9299e985263542f9802ead2e956eca572e1d8b807a3e2338fdd0dc/stage",
-      onCommand: ({ command, mode, genres, genre }) => {
+      onCommand: ({ command, mode, genres, generOrCategory }) => {
         if (command === "chooseGenre") {
           const foundGenre = genres.find(
-            (g) => g.name.toLowerCase() === genre.name.toLowerCase()
+            (g) => g.name.toLowerCase() === generOrCategory.name.toLowerCase()
           );
 
           if (foundGenre) {
