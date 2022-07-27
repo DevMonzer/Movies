b9aec8dab0f9f52ccee7e3c06f16912caf3b3e6a
Monzer Mohammad <dev.monzer@gmail.com>
2022-07-27 11:18:31 +0300

    Update styles.js

---
+++ src/components/FeaturedMovie/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) => ({
+  featuredCardContainer: {
+    marginBottom: "20px",
+    display: "flex",
+    justifyContent: "center",
+    height: "490px",
+    textDecoration: "none",
+  },
+}));
