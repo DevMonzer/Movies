93de603ba4b0f2216ff39ff3461354699e52884d
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-05 13:34:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,30 +14,28 @@ const App = () => {
   useAlan();
 
   return (
-    <>
-      <div className={classes.root}>
-        <CssBaseline />
-        <Navbar />
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Switch>
-            <Route exact path={["/", "/approved"]}>
-              <Movies />
-            </Route>
-            <Route exact path="/movie/:id">
-              <MovieInformation />
-            </Route>
-            <Route exact path="/actor/:id">
-              <Actors />
-            </Route>
-            <Route exact path="/profile/:id">
-              <Profile />
-            </Route>
-          </Switch>
-        </main>
-        <div ref={alanButoonContainer} />
-      </div>
-    </>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path={["/", "/approved"]}>
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actor/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+      <div ref={alanButoonContainer} />
+    </div>
   );
 };
 
