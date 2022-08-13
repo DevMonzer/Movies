48b4b0f2ddc74416d4d0059a5683608ecfb9ae18
Monzer Mohammad <dev.monzer@gmail.com>
2022-08-13 13:54:51 +0300

    Update Movies.jsx

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -23,7 +23,7 @@ const Movies = () => {
   const lg = useMediaQuery((theme) => theme.breakpoints.only("lg"));
   const numberOfMovies = lg ? 17 : 19;
 
-  console.log(genreIdOrCategoryName);`
+  // console.log(genreIdOrCategoryName);`
 
   if (isFetching) {
     return (
